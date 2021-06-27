3559c66d09fe3678763d281fab748d3ce58901d1
tuongtran23 <63479988+tuongtran23@users.noreply.github.com>
2021-06-27 23:07:44 +0000

    Part 3

--- candidate-testing.js
+++ candidate-testing.js
@@ -16,7 +16,7 @@ let candidateAnswer = "";
 let questions = 
 ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " , "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
-let candidateAnswers;
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -29,6 +29,7 @@ function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   for (let i =0; i<questions.length; i++){
     candidateAnswer= input.question(`${i+1}) ${questions[i]}`);
+    candidateAnswers.push(candidateAnswer);
     console.log("Your Answer: ", candidateAnswer);
     console.log("Correct Answer: ", correctAnswers[i]);
   }
@@ -42,7 +43,12 @@ function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
 
-  let grade;
+  let grade = 0;
+  for (let i = 0; i < questions.length; i++){
+    if (candidateAnswer[i] == correctAnswers[i]){
+      grade += 20;
+    }
+  }
   
 
   return grade;
